2bf5511c5d8e24a985d7d9eedbf53116282220e6
Shaojun-J <68138142+Shaojun-J@users.noreply.github.com>
2022-11-28 20:26:53 +0000

    test1

--- test.c
+++ test.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 
+//int Test (int a);
 void main (void)
 {
 	printf(" Hello world! \n");
 	printf(" 1111 \n");
 	printf(" 2222 \n");
 	printf("321\n");
+
+	int a,b,c;
+	a = 1;
+	b = 2;
+	c = 3;
+	printf("a=%d, b=%d, c=%d \n",a,b,c);
+	b = Test(a);
+	c += a;
+	printf("a=%d, b=%d, c=%d \n",a,b,c);
 	 
+}
+
+int Test (int a)
+{
+	return 2*(a+a);
 }
